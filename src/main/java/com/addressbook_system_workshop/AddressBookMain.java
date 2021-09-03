2fd4a0d91bdf66f94227ea1a5741ed394f20f578
vismayatk002 <vismayatk002@gmail.com>
2021-09-03 09:09:07 +0530

    Refactor create contact

--- src/main/java/com/addressbook_system_workshop/AddressBookMain.java
+++ src/main/java/com/addressbook_system_workshop/AddressBookMain.java
@@ -1,9 +1,65 @@
 package com.addressbook_system_workshop;
 
+import java.util.Scanner;
+
 public class AddressBookMain {
+	
+	
+	public void createContact() {
+		
+		PersonContact contact =new PersonContact();
+		Scanner sc= new Scanner(System.in);
+		
+		System.out.print("\nEnter First Name  : ");  
+        String firstName = sc.nextLine();
+        contact.setFirstName(firstName);
+
+        System.out.print("\nEnter Last Name  : ");  
+        String lastName = sc.nextLine(); 
+        contact.setLastName(lastName);
+
+        System.out.print("\nEnter E-mail  : ");
+        String email = sc.nextLine();
+        contact.setEmail(email);
+
+        System.out.print("\nEnter Address  : ");  
+        String address = sc.nextLine(); 
+        contact.setAddress(address);
+
+        System.out.print("\nEnter City  : ");  
+        String city = sc.nextLine(); 
+        contact.setCity(city);
+
+        System.out.print("\nEnter State  : ");  
+        String state = sc.nextLine();
+        contact.setState(state);
+        
+        System.out.print("\nEnter Phone Number  : ");   
+        long phoneNo = sc.nextLong();
+        contact.setPhoneNo(phoneNo);
+
+        System.out.print("\nEnter Zip  : ");  
+        int zip = sc.nextInt(); 
+        contact.setZip(zip);
+        showContact(contact);
+	}
+	
+	public void showContact(PersonContact contact){
+
+        System.out.print("\n-----------------");
+        System.out.print("\nFirst Name  : " +  contact.getFirstName());
+        System.out.print("\nLast Name   : " +  contact.getLastName());
+        System.out.print("\nAddress     : " +  contact.getAddress());
+        System.out.print("\nCity        : " +  contact.getCity());
+        System.out.print("\nState       : " +  contact.getState());
+        System.out.print("\nPone Number : " +  contact.getPhoneNo());
+        System.out.print("\nE-mail      : " +  contact.getEmail());
+        System.out.print("\nZip         : " +  contact.getZip());
+    }
 	
     public static void main( String[] args ){
     	
-        System.out.println( "Welcome to Address Book Program" );
+    	AddressBookMain book = new AddressBookMain();
+    	book.createContact();
     }
 }
---
+++ src/main/java/com/addressbook_system_workshop/PersonContact.java
@@ -0,0 +1,57 @@
+package com.addressbook_system_workshop;
+
+public class PersonContact {
+
+	String firstName,lastName,address,city,state,email;
+	int zip;
+	long phoneNo;
+	
+	public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+    public String getFirstName(){
+        return firstName;
+    }
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+    public String getLastName(){
+        return lastName;
+    }
+    public void setAddress(String address){
+        this.address = address;
+    }
+    public String getAddress(){
+        return address;
+    }
+    public void setCity(String city){
+        this.city = city;
+    }
+    public String getCity(){
+        return city;
+    }
+    public void setState(String state){
+        this.state = state;
+    }
+    public String getState(){
+        return state;
+    }
+    public void setPhoneNo(long phoneNo){
+        this.phoneNo = phoneNo;
+    }
+    public long getPhoneNo(){
+        return phoneNo;
+    }
+    public void setZip(int zip){
+        this.zip = zip;
+    }
+    public int getZip(){
+        return zip;
+    }
+    public void setEmail(String email){
+        this.email = email;
+    }
+    public String getEmail(){
+        return email;
+    }
+}
